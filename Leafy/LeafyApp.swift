576a571f84e67b4e34573ac3dbc89dffde0df5ba
DinachiO <Favourdinachi@gmail.com>
2025-10-31 12:43:43 -0400

    Initial Commit

---
+++ Leafy/LeafyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafyApp.swift
+//  Leafy
+//
+//  Created by Emeka prince amobi on 2025-10-31.
+//
+
+import SwiftUI
+
+@main
+struct LeafyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
